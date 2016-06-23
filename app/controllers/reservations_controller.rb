11b28275234d6332f9865c67e4f60ba601d1e6a9
Enakshi Shah <enakshi.shah@mail.utoronto.ca>
2016-06-23 13:58:48 -0400

    added update, delete, and edit methods for reservation controller

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -17,15 +17,28 @@ def create
 end
 
 def show
+  @reservation = Reservation.ind(params[:id])
 end
 
 def delete
+  @reservation = Reservation.find(params[:id])
+  @reservation.destroy
+  redirect_to reservations_path
 end
 
 def edit
+  @reservation = Reservation.find(params[:id])
 end
 
 def update
+  @reservation = Reservation.find(params[:id])
+  if @reservation.update_attributes(reservation_params)
+    flash[:notice] = "The reservation has been updated."
+    redirect_to reservations_path
+  else
+    flash[:alert] = "The reservation was not updated successfully"
+    render :edit
+  end
 end
 
 private
