6f3920ec0d3c57f7f2a3be5094abbabc8c39092c
Leon Chiu <leontkchiu@gmail.com>
2016-06-23 13:46:01 -0400

    added user controller, created user routes for new and create

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ActiveRecord::Base
   has_many :reserved_restaurants, through: :reservations, source: :restaurant
   has_many :owned_restaurants, class_name: "Restaurant"
 
+  has_secure_password
+
 end
