baebfdb8bd381669620308ea0e934adcc62bafff
Leon Chiu <leontkchiu@gmail.com>
2016-06-23 14:17:18 -0400

    log in functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-
   root 'users#new'
 
   resources :users, only: [:new, :create]
   resources :restaurants
+  resource :sessions, only: [:new, :create, :destroy]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
