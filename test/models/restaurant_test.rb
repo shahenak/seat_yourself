9ad9ed0e940bbe014fb075d709a71cad30a10c55
Ji You <kroyagis@gmail.com>
2016-06-24 14:42:01 -0400

    added setup to restaurant_test, and refactored valid restaurant block

--- test/models/restaurant_test.rb
+++ test/models/restaurant_test.rb
@@ -1,15 +1,11 @@
 require 'test_helper'
 
 class RestaurantTest < ActiveSupport::TestCase
-  test "the truth" do
-    assert true
+  setup do
+    @restaurant = FactoryGirl.create :restaurant
   end
 
   test "valid restaurant" do
-    # Returns a User instance that's not saved
-    restaurant = build(:restaurant)
-    assert_equal true, restaurant.valid?
-    puts restaurant.name
-    puts restaurant.address
+    assert_equal true, @restaurant.valid?
   end
 end
