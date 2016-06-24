2a719b66f5c788065d5c5c004c5494ef33a706fb
Enakshi Shah <enakshi.shah@mail.utoronto.ca>
2016-06-24 15:30:46 -0400

    added validation for reserving seats

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,13 +2,24 @@ class Reservation < ActiveRecord::Base
   belongs_to :restaurant
   belongs_to :user
 
-def seats_taken
-  var = @reservations.select{|element| element[:restaurant_id] == params[:restaurant_id] && element[:time] == params[:time]}
-  var.map(&:size).reduce(:+)
-end
+  # validate :seats_taken
+  #validates_inclusion_of :size, in: 0..100
+  # validates size, :inclusion => { :in => r, :min => 1, :max => 10} }
+  # lambda { |foo| foo.allowed_types }
+  #validates_inclusion_of :size, in: lambda { |reservation| 1..reservation.restaurant.capacity }
+  validates_inclusion_of :size, in: lambda { |reservation| 1..reservation.restaurant.seats_remaining }
 
-def seats_remaining
-  100 - seats_taken
-end
+
+# def seats_taken
+#   var = @reservations.select{|element| element[:restaurant_id] == params[:restaurant_id] && element[:time] == params[:time]}
+#   var.map(&:size).reduce(:+)
+# end
+
+
+# def  seats_taken
+#   if size > 10
+#     errors.add(:size)
+#   end
+# end
 
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,4 +3,8 @@ class Restaurant < ActiveRecord::Base
   has_many :users, through: :reservations
   belongs_to :owner, class_name: "User", foreign_key: :user_id
 
+
+def seats_remaining
+  100 - reservations.sum(:size)
+end
 end
