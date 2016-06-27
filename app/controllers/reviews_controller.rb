33c1ff8bb9211c222bf6386116eaa0320bf90e03
Ji You <kroyagis@gmail.com>
2016-06-27 16:08:21 -0400

    completed reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,10 +1,16 @@
 class ReviewsController < ApplicationController
+  before_action :ensure_logged_in, only: [:destroy, :create, :update]
+  before_action :load_restaurant
+  before_action :find_reservation, only: [:show, :edit, :update, :destroy]
+
+
   def new
     @review = Review.new
   end
 
   def create
-    @review = Review.new(review_params)
+    @review = @restaurant.reviews.build(review_params)
+    @review.user = current_user
     if @review.save
       flash[:notice] = "Review was successfully created."
       redirect_to restaurants_url
@@ -25,5 +31,12 @@ class ReviewsController < ApplicationController
     params.require(:review).permit(:comment)
   end
 
+  def load_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+  def find_review
+    @reservation = Reservation.find(params[:id])
+  end
 
 end
